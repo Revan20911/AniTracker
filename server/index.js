181be6827fd1c700459aac99b9a557336cac79c3
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-09-29 19:58:29 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ mongoose.Promise = global.Promise;
 const path = require("path");
 
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
 })
 
 
