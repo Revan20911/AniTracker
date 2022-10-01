48279d447a7a855ef819cc1fd56b706bddbe6dcf
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-10-01 12:25:38 -0500

    Update series.js

--- client/src/components/pages/series.js
+++ client/src/components/pages/series.js
@@ -3,14 +3,64 @@ import "../styles/series.css"
 import { useParams } from "react-router-dom";
 
 export default function Series(){
-    const {id, title} = useParams();
+    const {title} = useParams();
+
+    const [info, setInfo] = React.useState([]);
+
+    React.useEffect(()=>{
+        async function fetchSeriesInfo(){
+            const response = await fetch(`http://localhost:5000/anime/series/${id}`)
+            const seriesInfo = await response.json();
+
+            setInfo(seriesInfo);
+        }
+        fetchSeriesInfo();
+    })
+
     return(
         <div className="series-wrapper">
-            <h1>{title}</h1>
+            <h1>{info.title}</h1>
             <div className="series-sub-content-wrapper">
-                <div className="series-sub-content"></div>
+                <div className="series-sub-content">
+                    <img src={info.img}/>
+                    <h4>Information</h4>
+                    <div className="series-information">
+                        <div className="item-info">Episodes:  
+                        <span className="info-text">  
+                        {info.episodes}
+                        </span>
+                    </div>
+                    </div>
+                    <h4>Statistics</h4>
+                    <h4>External Links</h4>
+                </div>
+                <div className="info-wrapper">
+                <div className="ranking">
+                <div className="stat-score">
+                        </div>
+                    <div className="ranking-stats">
+                       
+                        <div className="stat-titles">
+                        <h4>Ranked: N/A</h4>
+                        <h4>Popularity: N/A</h4>
+                        <h4>Members: N/A</h4>
+                        </div>
+                    </div>
+                    <iframe className="ranking-pv" src="https://www.youtube.com/embed/WFVY88Urzuc?enablejsapi=1&wmode=opaque&autoplay=1"></iframe>
+                </div>
+                <div className="description">
+                    <h3>Synopsis</h3>
+                    <p>{info.desc}</p>
+                </div>
+                <div className="description">
+                    <h3>Characters & Voice actors</h3>
+                </div>
+                <div className="description">
+                    <h3>Reviews</h3>
+                    <h3>Images</h3>
+                </div>
+                </div>
             </div>
-            
         </div>
     )
 }
