e64210b04f43698d07eee7ee969272d70176988a
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-10-01 14:27:00 -0500

    Update register.js

--- client/src/components/pages/register.js
+++ client/src/components/pages/register.js
@@ -21,7 +21,7 @@ export default function Register(){
         e.preventDefault();
         const newUser = {...user};
 
-        await fetch("http://localhost:5000/register" , {
+        await fetch("https://anime----tracker.herokuapp.com/register" , {
             method: 'POST',
             headers: {
                 "Content-Type" : "application/json",
